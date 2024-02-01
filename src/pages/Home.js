958f1c06a3b7a2b889f1c739ef7f63dd3916587e
Tunmise Atanda <75289869+Atanda-Oluwatunmise@users.noreply.github.com>
2024-02-01 12:31:14 +0100

    Style: Adds Input box.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import '../styles/Home.css' 
+//import BannerImage from '../assets/annie-spratt-yI3weKNBRTc-unsplash.jpg' 
+//import BannerImage from '../assets/codioful-formerly-gradienta-7brhZmwXn08-unsplash.jpg' 
 
 function Home() {
   return (
-    <div>
-      Hello world
+    <div className='home'>
+      <div className='headerContainer'>
+        <h1>Url Shortener</h1>
+        <p>Shorten that annoying long url</p>
+        <input name='myInput'/>
+        <div className='buttonDiv'>
+         <button>SHORTEN</button>
+        </div>
+      </div>
     </div>
   )
 }
